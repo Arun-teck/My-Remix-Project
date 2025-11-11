bd44be820413e202770ec6e1cd06afc5c49d9e50
Arun-Leo <aruns88843@gmail.com>
2025-11-11 13:14:26 +0530

    Add unicode bytes variable to ExampleStrings contract
    Added a new bytes variable to store unicode string.

--- ExampleStrings.sol
+++ ExampleStrings.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.30;
 
 contract ExampleStrings {
     string public myString = "Hello World"; 
+    bytes public myBytes = unicode"Hello World";
 
     function setMyString(string memory _myString) public {
         myString = _myString;
